a5a5ed8060a3431fa5a1fcc63ccf5aff9b1ae00a
Oleg Solonina <saloroler@gmail.com>
2018-02-12 16:02:12 +0200

    without problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { SearchUserComponent } from './components/search-user/search-user.component';
+
+import { GithubDataService } from "./services/github-data.service";
+import { AlertComponent } from './components/alert/alert.component';
+import { AppRootingModule } from './app-rooting.module';
+import { InfoComponent } from './components/info/info.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutAppComponent } from './components/about-app/about-app.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUserComponent,
+    AlertComponent,
+    InfoComponent,
+    HomeComponent,
+    AboutAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRootingModule
   ],
-  providers: [],
+  providers: [GithubDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
